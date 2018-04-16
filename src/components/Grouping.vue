8a3134d4f98d3643343c93a29bc9e83690c51836
k-tateishi <crazyweather0001@gmail.com>
2018-04-16 23:08:36 +0900

    add group list

--- src/components/Grouping.vue
+++ src/components/Grouping.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>
     <div class="columns">
       <div class="column">
         <label class="label">メンバー名</label>
@@ -21,36 +22,36 @@
       </div>
 
       <div class="column">
-        <label class="label">グループ名</label>
-        <div class="field has-addons">
-          <div class="control has-icons-left">
-            <input class="input" type="text" placeholder="グループA" v-model="groupName">
-            <span class="icon is-small is-left">
-              <i class="far fa-user"></i>
-            </span>
-          </div>
-          <div class="control">
-            <a class="button is-info" @click="addGroup">
-              追加
-            </a>
-          </div>
-        </div>
-        <div v-for="groupName in groupsList" :key="groupName">
-          <div v-show="groupName">{{ groupName }} <a class="delete" @click="deleteGroup(groupName)"></a></div>
-        </div>
-      </div>
-
-      <div class="column">
         <label class="label">グループ数</label>
         <input class="input" type="text" placeholder="3" v-model="groupNumber">
       </div>
 
       <div class="column">
         <label class="label">&nbsp;</label>
-        <button class="button is-info" v-show="inputValidation">この条件で班分けする！</button>
+        <button class="button is-info" v-show="inputValidation" @click="makeGroup">この条件で班分けする！</button>
         <button class="button is-info" v-show="!inputValidation" disabled>条件を設定してください</button>
       </div>
     </div>
+
+    <!-- TODO: カラム対応、色分け -->
+    <div v-for="group in makedGroupsList" :key="group">
+      <div class="card">
+        <header class="card-header">
+          <p class="card-header-title">
+            {{ group }}
+          </p>
+        </header>
+        <div class="card-content">
+          <div class="content" v-for="groupedUser in groupedUsersList(group)" :key="groupedUser.id">
+            <p>{{ groupedUser.user }}</p>
+          </div>
+        </div>
+        <footer class="card-footer">
+        </footer>
+      </div>
+    </div>
+    <!-- TODO: CSV表示 -->
+  </div>
 </template>
 
 <script>
@@ -60,9 +61,9 @@
       return {
         users: [],
         userName: "",
-        groups: [],
-        groupName: "",
-        groupNumber: null
+        groupNumber: null,
+        makedGroups: [],
+        groupedUsers: []
       }
     },
     methods: {
@@ -82,41 +83,74 @@
         });
         this.users = deleted;
       },
-      addGroup() {
-        if (this.groups.indexOf(this.groupName) >= 0) {
-          // TODO: エラーメッセージを出す
-          return;
+      initializeGroupNumber() {
+        if (!this.validNumber || this.groupNumber === 0) {
+          this.groupNumber = 1;
         }
-        this.groups.push(this.groupName);
-        this.groupName = "";
       },
-      deleteGroup(name) {
-        var deleted = this.groups.filter((e) => {
-          if (e !== name) {
+      makeGroup() {
+        // Group names
+        this.initializeGroupNumber();
+        for (var i=1; i<=this.groupNumber; i++) {
+          this.makedGroups.push("グループ" + i);
+        }
+
+        // Make random users
+        var userIndex = this.users.length;
+        var randomUserIndex = [];
+        var indexList = [];
+        for (var j=0; j<userIndex; j++) {
+          indexList.push(j);
+        }
+        for (var k=0; k<this.users.length; k++) {
+          var rand = Math.floor(Math.random() * userIndex);
+          userIndex -= 1;
+          var temp = indexList[userIndex];
+          indexList[userIndex] = indexList[rand];
+          indexList[rand] = temp;
+          randomUserIndex.push(indexList[userIndex]);
+        }
+
+        // Grouped users
+        var groupNumberIndex = 1;
+        for (var l=0; l<this.users.length; l++) {
+          var groupedUser = {};
+          groupedUser.id = l;
+          groupedUser.group = "グループ" + groupNumberIndex;
+          groupedUser.user = this.users[randomUserIndex[l]];
+          this.groupedUsers.push(groupedUser);
+          groupNumberIndex++;
+          if (groupNumberIndex > this.groupNumber) {
+            groupNumberIndex = 1;
+          }
+        }
+      },
+      groupedUsersList(groupName) {
+        if (!groupName || groupName.length <= 0) {
+          return [];
+        }
+        return this.groupedUsers.filter((e) => {
+          if (e && e.group === groupName) {
             return e;
           }
         });
-        this.groups = deleted;
       }
     },
     computed: {
       usersList: function() {
         return this.users;
       },
-      groupsList: function() {
-        return this.groups;
-      },
       validUsers: function() {
         return this.users && this.users.length > 0;
-      },
-      validGroups: function() {
-        return this.groups && this.groups.length > 0;
       },
       validNumber: function() {
         return this.groupNumber && this.groupNumber.search(/^[0-9]+$/) === 0;
       },
       inputValidation: function() {
-        return this.validUsers && this.validGroups && this.validNumber;
+        return this.validUsers && this.validNumber;
+      },
+      makedGroupsList: function() {
+        return this.makedGroups;
       }
     }
   }
