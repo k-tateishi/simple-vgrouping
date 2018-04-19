9417193a5f323612702d9d98375de3a1fdc6b7a9
k-tateishi <crazyweather0001@gmail.com>
2018-04-19 22:55:16 +0900

    fix footer message

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,7 @@
         <div class="content has-text-centered">
           <p>
             <span>
-              Copyright <i class="far fa-copyright"></i> {{ year }} k-tateishi All Rights Reserved.
+              {{ year }} simple-vgrouping
             </span>
           </p>
         </div>
